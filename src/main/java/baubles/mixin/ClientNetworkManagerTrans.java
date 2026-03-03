e1bc1dfad6787252ad47c37063ddc228dac468f0
Inf1nity <1449896523@qq.com>
2026-03-03 17:51:29 +0800

    Fix

--- src/main/java/baubles/mixin/ClientNetworkManagerTrans.java
+++ src/main/java/baubles/mixin/ClientNetworkManagerTrans.java
@@ -5,44 +5,46 @@ import baubles.common.lib.PlayerHandler;
 import baubles.common.network.BaublesNetHandler;
 import baubles.common.network.PacketOpenBaublesInventory;
 import baubles.common.network.SPacketSyncBauble;
+import baubles.gui.GuiPlayerExpanded;
 import baubles.imixin.EntityPlayerAccessor;
 import net.minecraft.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(NetClientHandler.class)
 public abstract class ClientNetworkManagerTrans extends NetHandler implements BaublesNetHandler {
 	@Shadow private Minecraft mc;
 
-	@Unique
-	private boolean baubles$hasReceivedSync = false;
-
 	@Override
-    public void baubles$handleSyncBauble(SPacketSyncBauble packet) {
-        World world = Minecraft.getMinecraft().theWorld;
+	public void baubles$handleSyncBauble(SPacketSyncBauble packet) {
+		World world = Minecraft.getMinecraft().theWorld;
 		if (world == null) return;
 		Entity entity = world.getEntityByID(packet.playerId);
-		if (entity instanceof EntityPlayer player) {
+		if (!(entity instanceof EntityPlayer player)) return;
 
-			if (!baubles$hasReceivedSync) {
-				PlayerHandler.clearPlayerBaubles(player);
-				baubles$hasReceivedSync = true;
-			}
 
-			if (this.mc.thePlayer.openContainer instanceof ContainerPlayerExpanded containerPlayerExpanded) {
-				containerPlayerExpanded.baubles.stackList[packet.slot] = packet.bauble;
-			}
-			// Update PlayerHandler HashMap with synced data from server
-			PlayerHandler.getPlayerBaubles(player).stackList[packet.slot] = packet.bauble;
+		PlayerHandler.getPlayerBaubles(player).stackList[packet.slot] = packet.bauble;
+
+		if (mc.currentScreen instanceof GuiPlayerExpanded gui) {
+			ContainerPlayerExpanded container = (ContainerPlayerExpanded) gui.inventorySlots;
+			container.baubles.blockEvents = true;
+			container.baubles.stackList[packet.slot] = packet.bauble;
+			container.baubles.blockEvents = false;
 		}
-    }
+	}
 
+	@Override
 	public void baubles$handlerOpenBaublesInventory(PacketOpenBaublesInventory packet) {
-		// Reset sync flag when opening inventory to ensure fresh sync
-		baubles$hasReceivedSync = false;
 		((EntityPlayerAccessor)(Object) this.mc.thePlayer).baubles$displayGuiPlayerBaubles();
 		this.mc.thePlayer.openContainer.windowId = packet.windowId;
+
+		if (mc.currentScreen instanceof GuiPlayerExpanded gui) {
+			ContainerPlayerExpanded container = (ContainerPlayerExpanded) gui.inventorySlots;
+			ItemStack[] src = PlayerHandler.getPlayerBaubles(this.mc.thePlayer).stackList;
+			container.baubles.blockEvents = true;
+			System.arraycopy(src, 0, container.baubles.stackList, 0, src.length);
+			container.baubles.blockEvents = false;
+		}
 	}
 
 }
